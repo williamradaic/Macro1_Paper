25f2b6532166955a6314f8d4e0cc474213f79792
williamradaic <52841555+williamradaic@users.noreply.github.com>
2020-09-12 03:43:13 -0300

    Credit % GDP, cross country OK.

--- Macro1_Paper_Plots.R
+++ Macro1_Paper_Plots.R
@@ -94,11 +94,11 @@ names(gdp_demand.df) <- c("GDP","C", "I", "G", "X", "M")
 
 gdp_demand.plot <- plot_ly(gdp_demand.df, x = ~(2007:2017), y = ~C, type = "bar", name = "Consumo")
 
-gdp_demand.plot <- gdp_demand.plot  %>% add_trace(y = ~I, name = "Investimento") %>% add_trace(y = ~G, name = "Gastos do Governo") %>% add_trace(y = ~(X-M), name = "Exportações Líquidas")  %>% add_trace(y = ~GDP, type = "scatter", mode = "line", name = "PIB") %>% layout(xaxis = list(title = "Tempo", dtick = 1, tickmode = "linear"), font = list(family = "Helvetica", size = 20), yaxis = list(title = "Crescimento anual (%)"), barmode = "group", legend = list(title = list(text = "<b> Variável <b>")))
+gdp_demand.plot <- gdp_demand.plot  %>% add_trace(y = ~I, name = "Investimento") %>% add_trace(y = ~G, name = "Gastos do Governo") %>% add_trace(y = ~(X-M), name = "Exportações Líquidas")  %>% add_trace(y = ~GDP, type = "scatter", mode = "line", name = "PIB") %>% layout(xaxis = list(title = "Tempo", dtick = 1, tickmode = "linear"), font = list(family = "Helvetica", size = 20), yaxis = list(title = "Variação anual (%)"), barmode = "group", legend = list(title = list(text = "<b> Variável <b>")))
 
 # Exports x Imports plot (%)
 
-exp_imp.plot <- plot_ly(gdp_demand.df, x = ~(2007:2017), y = ~X, type = "bar", name = "Exportações") %>% add_trace(y = ~(-M), name = "Importações") %>% layout(xaxis = list(title = "Tempo", dtick = 1, tickmode = "linear"), font = list(family = "Helvetica", size = 20), yaxis = list(title = "Crescimento anual (%)"), barmode = "overlay", legend = list(title = list(text = "<b> Variável <b>")))
+exp_imp.plot <- plot_ly(gdp_demand.df, x = ~(2007:2017), y = ~X, type = "bar", name = "Exportações") %>% add_trace(y = ~(-M), name = "Importações") %>% layout(xaxis = list(title = "Tempo", dtick = 1, tickmode = "linear"), font = list(family = "Helvetica", size = 20), yaxis = list(title = "Variação anual (%)"), barmode = "overlay", legend = list(title = list(text = "<b> Variável <b>")))
 
 # GDP components: Demand (absolute)
 
@@ -115,18 +115,53 @@ gdp_demand_abs.plot <- gdp_demand_abs.plot  %>% add_trace(y = ~I, name = "Invest
 exp_imp_abs.plot <- plot_ly(gdp_demand_abs.df, x = ~(2007:2017), y = ~X, type = "bar", name = "Exportações") %>% add_trace(y = ~(-M), name = "Importações") %>% layout(xaxis = list(title = "Tempo", dtick = 1, tickmode = "linear"), font = list(family = "Helvetica", size = 20), yaxis = list(title = "USD (Valores de 2017)"), barmode = "overlay", legend = list(title = list(text = "<b> Variável <b>")))
 
 
+# GDP components: supply-side (%)
 
-
-# GDP components: supply-side
-
-gdp_supply.df <- data.frame(wb.clean.num$NY.GDP.MKTP.CD, wb.clean.num$NV.AGR.TOTL.CD, wb.clean.num$NV.IND.TOTL.CD, wb.clean.num$NV.SRV.TOTL.CD, wb.clean.num$NV.IND.MANF.CD)
+gdp_supply.df <- data.frame(wb.clean.num$NY.GDP.MKTP.KD.ZG, wb.clean.num$NV.AGR.TOTL.KD.ZG, wb.clean.num$NV.IND.TOTL.KD.ZG, wb.clean.num$NV.SRV.TOTL.KD.ZG, wb.clean.num$NV.IND.MANF.KD.ZG)
 
 names(gdp_supply.df) = c("GDP", "Agricultura", "Indústria", "Serviços", "Manufatura")
 
-gdp_supply.plot <- plot_ly(gdp_supply.df, x = ~(2007:2017), y = ~Agricultura, type = "bar", name = "Agricultura") %>% add_trace(y = ~Indústria, name = "Indústria") %>% add_trace(y = ~Serviços, name = "Serviços") %>% add_trace(y = ~Manufatura, name = "Manufatura") %>% add_trace(y = ~GDP, name = "PIB", type = "scatter", mode = "line") %>% layout(xaxis = list(title = "Tempo", dtick = 1, tickmode = "linear"), font = list(family = "Helvetica", size = 20), yaxis = list(title = "USD (Valores de 2017)"), barmode = "stack", legend = list(title = list(text = "<b> Variável <b>"))) # tá dando divergencia com GDP total. Falta algum componente, provavelmente negativo.
+gdp_supply.plot <- plot_ly(gdp_supply.df, x = ~(2007:2017), y = ~Agricultura, type = "bar", name = "Agricultura") %>% add_trace(y = ~Indústria, name = "Indústria") %>% add_trace(y = ~Serviços, name = "Serviços") %>% add_trace(y = ~Manufatura, name = "Manufatura") %>% add_trace(y = ~GDP, name = "PIB", type = "scatter", mode = "line") %>% layout(xaxis = list(title = "Tempo", dtick = 1, tickmode = "linear"), font = list(family = "Helvetica", size = 20), yaxis = list(title = "Variação anual (%)"), barmode = "group", legend = list(title = list(text = "<b> Variável <b>"))) # Na % não da pra ver a pequena divergencia.
+
+# GDP components: supply-side (absolute)
+
+gdp_supply_abs.df <- data.frame(wb.clean.num$NY.GDP.MKTP.CD, wb.clean.num$NV.AGR.TOTL.CD, wb.clean.num$NV.IND.TOTL.CD, wb.clean.num$NV.SRV.TOTL.CD, wb.clean.num$NV.IND.MANF.CD)
+
+names(gdp_supply_abs.df) = c("GDP", "Agricultura", "Indústria", "Serviços", "Manufatura")
+
+gdp_supply_abs.plot <- plot_ly(gdp_supply_abs.df, x = ~(2007:2017), y = ~Agricultura, type = "bar", name = "Agricultura") %>% add_trace(y = ~Indústria, name = "Indústria") %>% add_trace(y = ~Serviços, name = "Serviços") %>% add_trace(y = ~Manufatura, name = "Manufatura") %>% add_trace(y = ~GDP, name = "PIB", type = "scatter", mode = "line") %>% layout(xaxis = list(title = "Tempo", dtick = 1, tickmode = "linear"), font = list(family = "Helvetica", size = 20), yaxis = list(title = "USD (Valores de 2017)"), barmode = "stack", legend = list(title = list(text = "<b> Variável <b>"))) # tá dando divergencia com GDP total. Falta algum componente, provavelmente negativo.
 
 
+# Credit % GDP plot.
 
+credit_gdp.df <- data.frame(wb.clean.num$FS.AST.DOMS.GD.ZS)
+
+names(credit_gdp.df) = "Credit"
+
+credit_gdp_world.df <- read_excel("C:/Users/William/Downloads/API_FS.AST.DOMS.GD.ZS_DS2_en_excel_v2_1353420.xls", 
+                                  skip = 2)
+
+credit_gdp_world.df_t = transpose(credit_gdp_world.df)
+
+rownames(credit_gdp_world.df_t) = colnames(credit_gdp_world.df)
+colnames(credit_gdp_world.df_t) = credit_gdp_world.df_t[1,]
+
+wb.cred.clean = credit_gdp_world.df_t[(52:62),]
+
+wb.cred.num = matrix(NA, nrow = 11, ncol = 264)
+
+for (i in (1:11)) for (j in (1:264)) {
+  
+  wb.cred.num[i,j] = as.numeric(wb.cred.clean[i,j])
+  
+}
+
+wb.cred.num = data.frame(wb.cred.num)
+
+rownames(wb.cred.num) = rownames(wb.cred.clean)
+colnames(wb.cred.num) = colnames(wb.cred.clean)
+
+credit_gdp.plot <- plot_ly(wb.cred.num, x = ~(2007:2017), y = ~`South Africa`, type = "scatter", mode = "line", name = "África do Sul") %>% add_segments(y = 100, yend = 100, x = 2007, xend = 2017, showlegend = FALSE, line = list(color = 'rgb(0, 0, 0)', width = 4, dash = 'dash')) %>% add_trace(y = ~`Argentina`, name = "Argentina") %>% add_trace(y = ~`United States`, name = "Estados Unidos")  %>% add_trace(y = ~`Chile`, name = "Chile") %>% add_trace(y = ~`Mexico`, name = "México") %>% layout(xaxis = list(title = "Tempo", dtick = 1, tickmode = "linear"), font = list(family = "Helvetica", size = 20), yaxis = list(title = "Crédito fornecido pelo setor financeiro (% PIB)"), barmode = "stack", legend = list(title = list(text = "<b> País <b>"))) 
 
 
 
